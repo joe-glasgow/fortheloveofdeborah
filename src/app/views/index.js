71e649740e7def852b714fffc823669ed3db2ee9
Joseph Morrison <josephmorrison@josephs-mbp-2.home>
2017-11-10 22:14:00 +0000

    updates

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -5,4 +5,4 @@ export { default as Home }            from './home/Home';
 export { default as AddAMemory }    from './addAMemory/AddAMemory';
 export { default as ThankYou }    from './thankyou/ThankYou';
 export { default as Memories }    from './memories/Memories';
-export { default as PageNotFound }    from './PageNotFound/PageNotFound';
+export { default as PageNotFound }    from './pageNotFound/PageNotFound';
